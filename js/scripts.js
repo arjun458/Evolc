fee82d0f5cc6b576ed4712b76799622b966fc3de
arjun458 <arjunpd2001@gmail.com>
2023-09-25 21:10:04 +0530

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -99,6 +99,30 @@ $(document).ready(function(){
 
 });
 
+//logo slide
+$(document).ready(function(){
+    $('.partner-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        },{
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
+
 //send main
 function sendMail(){
     var params = {
